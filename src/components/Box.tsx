cd57d7bb2308fcbf9423de527040cffc6b172c14
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-17 18:16:25 -0300

    Refactor Box component to support scrollable content and handle keyboard avoiding

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
 import { useAppSafeArea } from '@/hooks';
 import { colors } from '@/theme/colors';
 
 interface Props {
   children: React.ReactNode;
+  scrollable?: boolean;
 }
 
-export function Box({ children }: Props) {
+export function Box({ children, scrollable = true }: Props) {
   const { top, bottom } = useAppSafeArea();
 
+  const Container = scrollable ? ScrollView : View;
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.bg,
-        paddingHorizontal: 20,
-        paddingTop: top,
-        paddingBottom: bottom,
-      }}>
-      {children}
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Container
+        style={{
+          flex: 1,
+          backgroundColor: colors.bg,
+          paddingHorizontal: 20,
+          paddingTop: top,
+          paddingBottom: bottom,
+        }}
+        bounces={false}
+        showsVerticalScrollIndicator={false}>
+        {children}
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
