1886d647a196eabfe830308e99efd971ecfb21b4
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-06-22 19:45:43 -0300

    refactor: Improve button and text input components

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -2,16 +2,29 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Image, ImageBackground, Text, View } from 'react-native';
+import Toast from 'react-native-toast-message';
 
+import { useLogin } from '@/api/useCases/Login/useLogin';
 import Background from '@/assets/Background.jpg';
 import Omni from '@/assets/Logo.png';
 import { CustomButton } from '@/components';
 import { FormPasswordInput } from '@/components/Form/FormPasswordInput';
 import { FormTextInput } from '@/components/Form/FormTextInput';
+import { useAppSafeArea } from '@/hooks';
 import { AuthScreenProps } from '@/routes';
 import { LoginScheema, loginScheema } from '@/schemas/LoginSchema';
 
 export function LoginScreen({ navigation }: AuthScreenProps<'LoginScreen'>) {
+  const { mutate, isPending } = useLogin(() =>
+    Toast.show({
+      type: 'success',
+      text1: 'Login efetuado com sucesso!',
+      position: 'top',
+    })
+  );
+
+  const { top } = useAppSafeArea();
+
   const { control, handleSubmit, reset } = useForm<LoginScheema>({
     resolver: zodResolver(loginScheema),
     defaultValues: {
@@ -22,13 +35,16 @@ export function LoginScreen({ navigation }: AuthScreenProps<'LoginScreen'>) {
   });
 
   function handleLogin(data: LoginScheema) {
-    console.log(data);
+    mutate(data);
+
     reset();
   }
 
   return (
     <ImageBackground source={Background} className="flex-1">
-      <View className="flex-1 rounded-2xl bg-black/60 bg-opacity-50 p-5">
+      <View
+        className="flex-1 rounded-2xl bg-black/60 bg-opacity-50 p-5"
+        style={{ paddingTop: top }}>
         <Image source={Omni} className="-mx-5 mt-32 h-40 w-full self-center" resizeMode="contain" />
 
         <View className="mt-20 flex-1 items-center gap-y-2">
@@ -42,7 +58,7 @@ export function LoginScreen({ navigation }: AuthScreenProps<'LoginScreen'>) {
         </View>
 
         <View className="w-80 flex-grow self-center">
-          <CustomButton title="Entrar" onPress={handleSubmit(handleLogin)} />
+          <CustomButton title="Entrar" onPress={handleSubmit(handleLogin)} isDisabled={isPending} />
         </View>
       </View>
     </ImageBackground>
